8294d0bbb1acfb5634a0876c062062486b7248f8
Aleks645 <stu2213811105@uni-plovdiv.bg>
2025-01-31 19:04:58 +0200

    update .gitignore

--- AppStore.Models/obj/Debug/net8.0/AppStore.Models.AssemblyInfo.cs
+++ AppStore.Models/obj/Debug/net8.0/AppStore.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppStore.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46748b1258c095ec1c729ad5447adebceccb6139")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppStore.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppStore.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
